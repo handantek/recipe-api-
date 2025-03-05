b2d7064aa6adabc7812ff7d8bc2d4df290d5d5b4
handantek <handantekinogluspoti@gmail.com>
2025-03-05 13:37:11 +0300

    api-documentation done

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User' #settings auth_user_model configration
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_SCHEMA_CLASS' : 'drf_spectacular.openapi.AutoSchema',
+}
